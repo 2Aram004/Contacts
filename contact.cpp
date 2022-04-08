c6110694266f4d22b2ffd60acb7749d4b12e1315
2Aram004 <90963558+2Aram004@users.noreply.github.com>
2022-04-08 16:24:41 +0400

    Add files via upload

---
+++ contact.cpp
@@ -0,0 +1,169 @@
+#include "contact.h"
+
+using FP = void(Contact::*)(std::unordered_map<std::string, Contact::Datas>::iterator);
+
+Contact::Contact() noexcept
+{
+	user_command.emplace("add", &Contact::Add);
+	user_command.emplace("read", &Contact::Read);
+	user_command.emplace("delete", &Contact::Delete);
+	user_command.emplace("change", &Contact::Change);
+	
+	changed_data.emplace("name", &Contact::ChangeName);
+	changed_data.emplace("lastname", &Contact::ChangeLastName);
+	changed_data.emplace("address", &Contact::ChangeLastName);
+	changed_data.emplace("number", &Contact::ChangeLastName);
+
+	Start();
+}
+
+void Contact::Start()
+{
+	fout.open("Contact.txt", std::ofstream::app);
+	CommandList();
+	GetDatas();
+	Save();
+}
+
+void Contact::ChangeName(std::unordered_map<int, Datas>::iterator it)
+{
+	std::cin >> it->second.name;
+	ChangeInFile();
+}
+
+void Contact::ChangeLastName(std::unordered_map<int, Datas>::iterator it)
+{
+	std::cin >> it->second.lastname;
+	ChangeInFile();
+}
+
+void Contact::ChangeNumber(std::unordered_map<int, Datas>::iterator it)
+{
+	std::cin >> it->second.number;
+	ChangeInFile();
+}
+
+void Contact::ChangeAddress(std::unordered_map<int, Datas>::iterator it)
+{
+	std::cin >> it->second.address;
+	ChangeInFile();
+}
+
+void Contact::ChangeInFile()
+{
+	fout.close();
+	fout.open("Contact.txt");
+
+	for(auto it = contacts.begin(); it != contacts.end(); ++it) {
+		fout << it->first << '\n';
+		fout << it->second.name << '\n';
+		fout << it->second.lastname << '\n';
+		fout << it->second.address << '\n';
+		fout << it->second.number << '\n';
+		        }
+	    fout.close();
+}
+
+void Contact::CommandList() const
+{
+	std::cout << "Enter add to add\n";
+	std::cout << "Enter change to change\n";
+	std::cout << "Enter delete to delete\n";
+	std::cout << "Enter read to read\n";
+	std::cout << "Enter stop to stop\n";
+}
+
+void Contact::Delete()
+{
+	std::cout << "Which contact do you want to delete  ";
+	std::cin >> delete_contact_number;
+
+	auto it = contacts.find(delete_contact_number);
+
+	if(it != contacts.end()) {
+		 		it->second.name = " ";
+		 		it->second.lastname = " ";
+		 		it->second.number = " ";
+		 		it->second.address = " ";
+			ChangeInFile();
+
+			}  else { std::cout << "Contact was not found"; }
+	}
+
+void Contact::Add()
+{
+	std::cout << "Enter name please ";
+	std::cin >> datas.name;
+
+	std::cout << "\nEnter lastname please ";
+	std::cin >> datas.lastname;
+
+	std::cout << "\nEnter phone number please ";
+	std::cin >> datas.number;
+
+	std::cout << "Enter address please ";
+	std::cin >> datas.address;
+
+		contacts.emplace(count, datas);
+++count;
+		ChangeInFile();
+}
+
+void Contact::Save()
+{
+	for(auto it = contacts.begin(); it != contacts.end(); ++it) {
+		fout << it->first << '\n';
+		fout << it->second.name << '\n';
+		fout << it->second.lastname << '\n';
+		fout << it->second.address << '\n';
+		fout << it->second.number << '\n';
+			}
+}
+
+void Contact::GetDatas()
+{
+	while(command != "stop") {
+
+	std::cout << "\n\nEnter your command please ";
+	std::cin >> command;
+
+    if(command == "stop") continue;
+	user_command[command];
+    
+	}
+}
+
+void Contact::Read()
+{
+	fin.open("Contact.txt");
+	while(!fin.eof()) {
+		getline(fin, str);
+		std::cout << str;
+		std::cout << '\n';
+			}
+	fin.close();
+}
+
+void Contact::Change()
+{
+	using fptr = void(Contact::*)(std::unordered_map<int, Contact::Datas>::iterator);
+    do {    
+	    std::cout << "\nWhich contact do you want do change(number) ";
+	    std::cin >> change_contact_number;
+
+    } while(change_contact_number <= count);
+
+        std::unordered_map<int, Contact::Datas>::iterator it = contacts.find(change_contact_number);
+
+		std::cout << "Which data do you want to change? ";
+		std::cin >> change_data;
+
+		fptr function = changed_data[change_data];
+		(this->*function)(it);
+}
+
+int main()
+{
+	Contact contact;
+	contact.Start();
+}
---
+++ contact.h
@@ -0,0 +1,55 @@
+#ifndef _CONTACT_H
+#define _CONTACT_H
+
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+
+
+class Contact
+{
+public:
+	Contact() noexcept;
+	void Start();
+	~Contact(){fout.close();}
+
+public:
+
+    struct Datas
+	{
+		std::string name;
+		std::string lastname;
+		std::string number;
+		std::string address;
+	};
+
+private:
+	void Add();
+	void Delete();
+	void Change();
+	void Read();
+	void GetDatas();
+	void Save();
+	void CommandList() const;
+	void ChangeInFile();
+	void ChangeName(std::unordered_map<int, Datas>::iterator);
+	void ChangeLastName(std::unordered_map<int, Datas>::iterator);
+	void ChangeAddress(std::unordered_map<int, Datas>::iterator);
+	void ChangeNumber(std::unordered_map<int, Datas>::iterator);
+
+	std::ofstream fout;
+	std::ifstream fin;
+	int count = 1;
+	std::string str;
+	std::string command;
+	Datas datas;
+	std::unordered_map<std::string, void(Contact::*)()> user_command;
+	std::unordered_map<int, Datas> contacts;
+	std::unordered_map<std::string, void(Contact::*)(std::unordered_map<int, Datas>::iterator)> changed_data;
+	int change_contact_number;
+	std::string change_data;
+	int delete_contact_number;
+};
+
+#endif
